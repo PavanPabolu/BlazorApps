2ea25585a361673f0e5034dfc2e134b1e0adaa6a
PavanPabolu <PavanPabolu.edu@gmail.com>
2024-06-24 23:47:40 +0530

    Add project files.

---
+++ BlazorApp.Client.ConsumeAPI/Services/WeatherForecastService.cs
@@ -0,0 +1,26 @@
+﻿using BlazorApp.Client.ConsumeAPI.Models;
+using System.Net.Http.Json;
+
+namespace BlazorApp.Client.ConsumeAPI.Services
+{
+    public class WeatherForecastService
+    {
+        private readonly HttpClient _httpClient;
+
+        public WeatherForecastService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<WeatherForecast[]> GetWeatherForecastsAsync()
+        {
+            var response = await _httpClient.GetAsync("/WeatherForecast");
+            response.EnsureSuccessStatusCode();
+            //return await response.Content.ReadAsStringAsync();
+
+            var res =  await _httpClient.GetFromJsonAsync<WeatherForecast[]>("/WeatherForecast");//"api/weatherforecast");
+            return res;
+        }
+
+    }
+}
